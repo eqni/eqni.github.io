965e025602bfe2bbc59242dbea5b90159cdb7675
Over Heaven <greenreedseedwood@gmail.com>
2023-10-04 12:17:27 -0600

    Added pointillism project

---
+++ 03-pointillism/sketch.js
@@ -0,0 +1,15 @@
+// Point Pirate
+// Andrew Chen
+// 10/4/2023
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+let worldmap = [];
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(220);
+}
