49aef2e302282afa0da46b08a4ce42aae8cace03
Over Heaven <greenreedseedwood@gmail.com>
2023-10-19 11:16:39 -0600

    Removed Images, Added and removed 2D array + invisible mouse
    Pixel art is too hard, 2D arrays freeze the screen. Custom mouse cursors are unconventional when the map only loads once. Instead I'll just use a square to symbolize the mouse's state.

--- 03-sandbox-2/sketch.js
+++ 03-sandbox-2/sketch.js
@@ -9,70 +9,31 @@ let snakeArray = [];
 let pixelSize = 4;
 let i;
 let cell = 0.01;
-let click;
+let mouse;
 
 let rotation = 0;
-let switchClock = 0;
-
-// Images
-let normalOff;
-let normalOn;
-
-function preload() {
-  normalOff = loadImage("snake.png");
-  normalOn = loadImage("selected-snake.png");
-}
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  drawMap();
+  createMap();
 }
 
 function draw() {
-  displaySnakes();
-  displayImages();
+  drawSnakes();
+  drawState();
 }
 
-function drawMap(){
-  for(let x = 0; x < windowWidth / pixelSize; x++) {
-    for(let y = 0; y < windowHeight / pixelSize; y++) {
-      fill(200);
-      rect(x * pixelSize, y * pixelSize, 4, 4);
+function createMap(){
+  fill(200);
+  for(let x = 0; x < windowWidth; x += 4) {
+    for(let y = 0; y < windowHeight; y += 4) {
+      rect(x, y, pixelSize, pixelSize);
     }
   }
 }
 
 function mousePressed() {
   spawnSnake();
-}
-
-function keyPressed() {
-  if (keyCode === 37) {
-    rotation--;
-    switchClock = 0;
-  }
-  if (keyCode === 39) {
-    rotation++;
-    switchClock = 0;
-  }
-}
-
-function displayImages() {
-  switchClock++;
-  if (switchClock < 100) {
-    normalOn.resize(100, 100);
-    normalOff.resize(100, 100);
-  } 
-  else {
-    normalOn.resize(20, 20);
-    normalOff.resize(20, 20);
-  }
-  if (rotation % 2 === 0) {
-    image(normalOn, 10, 10);
-  }
-  else {
-    image(normalOff, 10, 10);
-  }
 }
 
 function spawnSnake(){
@@ -95,7 +56,8 @@ function spawnSnake(){
       r: random(55, 255),
       g: random(55, 255),
       b: random(55, 255),
-      identity: "rgb"
+      identity: "rgb",
+      color: color(snake.r, snake.g, snake.b),
     };
   }
   else{
@@ -112,7 +74,7 @@ function spawnSnake(){
   snakeArray.push(snake);
 }
 
-function displaySnakes() {
+function drawSnakes() {
   for (i = 0; i < snakeArray.length; i++) {
     let snake = snakeArray[i];
     let rng = random();
@@ -120,7 +82,7 @@ function displaySnakes() {
       snake.r += random(1);
       snake.g += random(1);
       snake.b += random(1);
-      fill(snake.r % 255, snake.g % 255, snake.b % 255);
+      fill(snake.r, snake.g, snake.b);
     } 
     else {
       fill(snake.color);
@@ -139,8 +101,11 @@ function displaySnakes() {
     }
 
     rect(snake.x % windowWidth, snake.y % windowHeight, snake.s, snake.s);
-  
   }
+}
+
+function drawState(){
+  rect(0, 0, 10 * pixelSize, 10 * pixelSize);
 }
 
 
