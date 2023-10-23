196c637d8e17aa0f5cf18a75e9a78e641d75aa4b
Over Heaven <greenreedseedwood@gmail.com>
2023-10-23 12:20:11 -0600

    Added terrain-generation code

--- 08-terrain-generation/sketch.js
+++ 08-terrain-generation/sketch.js
@@ -1,15 +1,54 @@
-// Project Title
-// Your Name
-// Date
-//
-// Extra for Experts:
-// - describe what you did to take this project "above and beyond"
+// Terrain Generation
+// Oct 23, 2023
 
+let terrain = [];
+let xOffset = 0;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  spawnRectangles();
+  drawSquare();
 }
 
 function draw() {
   background(220);
+
+  if (keyIsDown(RIGHT_ARROW)) {
+    xOffset += 5;
+  }
+
+  if (keyIsDown(LEFT_ARROW)) {
+    if (xOffset > 5) {  //don't fall off the left side
+      xOffset -= 5;
+    }
+  }
+
+  displayRectangles();
+}
+
+function displayRectangles() {
+  for (let i = xOffset; i < width + xOffset; i++) {
+    let thisRect = terrain[i];
+    rect(thisRect.x - xOffset, height - thisRect.height, 1, thisRect.height);
+    fill(255);
+    rect(25, height - terrain[xOffset].height - 25, 25, 25);
+  }
+}
+
+function spawnRectangles() {
+  let time = 0;
+  for (let x = 0; x < 10000; x++) {
+    let h = noise(time) * height;
+    let thisRect = {
+      x: x,
+      height: h,
+    };
+    terrain.push(thisRect);
+    time += 0.001;
+  }
+}
+
+function drawSquare(){
+  fill(26);
+  rect(terrain[4].x, terrain[4].h, 10, 10);
 }
