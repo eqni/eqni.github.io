3282c8211c39bc6dbb539efff2424de56d6dc377
Over Heaven <greenreedseedwood@gmail.com>
2023-10-26 12:19:03 -0600

    Updated Warehouse Weed

--- 12-warehouse-weed/sketch.js
+++ 12-warehouse-weed/sketch.js
@@ -1,15 +1,49 @@
-// Project Title
-// Your Name
-// Date
-//
-// Extra for Experts:
-// - describe what you did to take this project "above and beyond"
+// Weed
 
+const GRID_SIZE = 60;
+
+let grid = [];
+let xOffset = 0;
+let yOffset = 0;
+let cellSize;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  cellSize = min(windowHeight, windowWidth) / GRID_SIZE;
+  grid = genMap(GRID_SIZE, GRID_SIZE);
 }
 
 function draw() {
-  background(220);
+  background(0);
+  drawMap(GRID_SIZE, GRID_SIZE);
+  resizeScale();
+}
+
+function genMap(rows, cols) {
+  let newMap = [];
+  for (let x = 0; x < rows; x++) {
+    newMap.push([]);
+    for (let y = 0; y < cols; y++) {
+      newMap[x].push(255);
+    }
+  }
+  return newMap;
+}
+
+function drawMap(rows, cols) {
+  for (let x = 0; x < rows; x++) {
+    for (let y = 0; y < cols; y++) {
+      fill(grid[x][y]);
+      rect(x * cellSize + xOffset, y * cellSize + yOffset, cellSize, cellSize);
+    }
+  }
+}
+
+function resizeScale() {
+  if (windowHeight >= windowWidth) {
+    yOffset = (windowHeight - windowWidth) / 2;
+  }
+  else if (windowHeight <= windowWidth) {
+    xOffset = (windowWidth - windowHeight) / 2;
+  }
 }
