f305e6546c4debeb0b98a8cfa318bb975d806932
Over Heaven <greenreedseedwood@gmail.com>
2023-10-31 11:46:06 -0600

    Reorganized map. Started money display

--- 12-warehouse-weed/sketch.js
+++ 12-warehouse-weed/sketch.js
@@ -9,15 +9,25 @@ let xOffset = 0;
 let yOffset = 0;
 let x;
 let y;
+let font;
+let money = 100;
+
+// Loads font
+function preload() {
+  font = loadFont("Pixel Font.TTF");
+}
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   resizeScale();
   noStroke();
   background(0);
+  textSize(30);
+  textFont(font);
   state = {
     identity: "normal",
-    size: [1.5, 1.5]
+    size: [1.5, 1.5],
+    direction: 0,
   };
   x = GRID_SIZE / 2;
   y = GRID_SIZE / 2;
@@ -48,37 +58,26 @@ function genMap(rows, cols) {
       // Dirt
       newMap[x][y] = color(random(110, 130), random(40, 50), random(25, 40));
 
-      // Road
-      if (y === 44) {
-        newMap[x][y] = color(random(35, 50));
-      }
-      else if (y > 38 && y < 44) {
-        newMap[x][y] = color(random(95, 115));
-      }
-      else if (y === 38) {
-        newMap[x][y] = color(random(35, 50));
-      }
-      if (y > 25 && x > 22 && x < 28) {
-        newMap[x][y] = color(random(95, 115));
-      }
-      else if (y > 25 && (y < 38 || y > 44) && x > 21 && x < 29) {
-        newMap[x][y] = color(random(35, 50));
-      }
-
-      // Road lines
-      if (y === 41 && x % 8 < 3 || x === 25 && y > 28 && y % 8 < 3) {
-        newMap[x][y] = color(random(245, 255), random(220, 240), random(0, 25));
-      }
-
       // Warehouse
-      if (y < 26 && y > 0) {
+      if (y < 34 && y > 0) {
         newMap[x][y] = color(random(220, 230));
       }
-      else if (y < 27) {
+      else if (y < 35) {
         newMap[x][y] = color(0);
       }
-      if (y === 26 && x > 22 && x < 28) {
+      if (y < 36 && y > 33 && x > 22 && x < 28) {
         newMap[x][y] = color(50, 84, 48, 50);
+      }
+      
+      // Road
+      if (x === 25 && y > 34 && y % 8 < 3) {
+        newMap[x][y] = color(random(245, 255), random(220, 240), random(0, 25));
+      }
+      else if (y > 34 && x > 22 && x < 28) {
+        newMap[x][y] = color(random(95, 115));
+      }
+      else if (y > 34 && x > 21 && x < 29) {
+        newMap[x][y] = color(random(35, 50));
       }
     }
   }
@@ -96,6 +95,7 @@ function drawMap(rows, cols) {
 
 function drawPlayer() {
   fill(0);
+  // Movement
   if (keyIsDown(39)) {
     x++;
   }
@@ -104,31 +104,45 @@ function drawPlayer() {
   }
   else if (keyIsDown(40)) {
     y++;
-    wallCheck();
+    state.direction = 0;
   }
   else if (keyIsDown(38)) {
     y--;
-    wallCheck();
+    state.direction = 1;
   }
+  gateCheck();
+
+  // State
   if (state.identity === "normal") {
     x = constrain(x, 1, 49);
-    y = constrain(y, 2, 25);
+    y = constrain(y, 2, 33);
     state.size = [1.5, 1.5];
     rect((x - state.size[0] / 2) * cellSize + xOffset, (y - state.size[1] / 2) * cellSize + yOffset, cellSize * state.size[0], cellSize * state.size[1]);
   }
   else if (state.identity === "truck") {
-    x = constrain(x, 1, 49);
-    y = constrain(y, 26, 50);
-    state.size = [1.5, 7.5];
+    x = constrain(x, 24, 27);
+    y = constrain(y, 33, 50);
+    state.size = [1.5, 4.5];
+    rect((x - state.size[0] / 2) * cellSize + xOffset, y * cellSize + yOffset, cellSize * state.size[0], cellSize * state.size[1]);
   }
-  rect((x - state.size[0] / 2) * cellSize + xOffset, (y - state.size[1] / 2) * cellSize + yOffset, cellSize * state.size[0], cellSize * state.size[1]);
 }
 
-function wallCheck() {
-  if(y < 25 && x > 22 && x < 28) {
+function gateCheck() {
+  if(y < 33 && x > 22 && x < 28) {
     state.identity = "normal";
   }
-  if(y > 25 && x > 22 && x < 28) {
+  if(y > 33 && x > 22 && x < 28) {
     state.identity = "truck";
   } 
+}
+
+function displayMoney() {
+  let tempMoney = money;
+  let temp;
+  let display = [];
+  for (let i = log(money); i > 0; i--) {
+    temp = floor(tempMoney / 10 ** i);
+    display.push(temp);
+    tempMoney -= tempMoney - temp * 10 ** i;
+  }
 }
