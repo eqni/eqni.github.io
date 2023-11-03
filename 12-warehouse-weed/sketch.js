79fe838a05e4cf8ee81df95106105b47c21a04e9
Over Heaven <greenreedseedwood@gmail.com>
2023-11-03 12:18:27 -0600

    Implemented Money Borders | Reorganized Functions

--- 12-warehouse-weed/sketch.js
+++ 12-warehouse-weed/sketch.js
@@ -1,5 +1,12 @@
-// Weed
-
+///////////////////////////////////////////////////////////////////////////////////////////
+// Warehouse Weed
+// Andrew Chen
+// 10/26/2023
+//
+// Extra for Experts:
+// - loaded fonts
+// - 3D Arrays (Questionable implementation)
+///////////////////////////////////////////////////////////////////////////////////////////
 const GRID_SIZE = 50;
 
 let maps = {
@@ -22,9 +29,8 @@ let x;
 let y;
 let font;
 let money = 100;
-let time;
 let plantGrowthStates = [[136, 8, 8], [170, 74, 68], [238, 75, 43], [255, 195, 0], [199, 234, 70], [152, 251, 152], [27, 131, 102]];
-let lobbyMap = [];
+
 // Loads font
 function preload() {
   font = loadFont("Pixel Font.TTF");
@@ -32,27 +38,16 @@ function preload() {
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  resizeScale();
-  noStroke();
   background(0);
-  textSize(30);
+  resizeScale();
+  textSize(32);
   textFont(font);
+
   x = GRID_SIZE / 2;
   y = GRID_SIZE / 2;
   cellSize = min(windowHeight, windowWidth) / GRID_SIZE;
-  maps.lobbyMap = genMap(GRID_SIZE, GRID_SIZE);
-  maps.data = genData(GRID_SIZE, GRID_SIZE);
-}
-
-function draw() {
-  time = millis();
-  drawMap(GRID_SIZE, GRID_SIZE);
-  drawPlayer();
-  rect(0, 0, 400, 100);
-  drawMoney();
-  if(time % 60 === 0) {
-    stimulatePlants();
-  }
+  maps.lobbyMap = genMap();
+  maps.data = genData();
 }
 
 function resizeScale() {
@@ -64,65 +59,59 @@ function resizeScale() {
   }
 }
 
-function genMap(rows, cols) {
-  let newMap = [];
+function mousePressed() {
+  let px = floor((mouseX - xOffset) / cellSize);
+  let py = floor((mouseY - yOffset) / cellSize);
+  if(maps.data[px][py][1] === 6) {
+    maps.data[px][py] = 0;
+    maps.lobbyMap[px][py] = color(random(220, 230));
+    money += 250;
+  }
+  else if (!(maps.data[px][py][0] === 2) && py > 0 && py < 34 && money >= 100){
+    maps.data[px][py] = [2, 0];
+    maps.lobbyMap[px][py] = color(plantGrowthStates[0]);
+    money -= 100;
+  }
+}
 
-  // lobbyMap Map
-  for (let x = 0; x < rows; x++) {
-    newMap.push([]);
-    for (let y = 0; y < cols; y++) {
-      newMap[x].push([]);
-
-      // Dirt
-      newMap[x][y] = color(random(110, 130), random(40, 50), random(25, 40));
-
-      // lobbyMap
-      if (y < 34 && y > 0) {
-        newMap[x][y] = color(random(220, 230));
-      }
-      else if (y < 35) {
-        newMap[x][y] = color(0);
-      }
-      if (y < 36 && y > 33 && x > 22 && x < 28) {
-        newMap[x][y] = color(50, 84, 48, 50);
-      }
-      
-      // Road
-      if (x === 25 && y > 34 && y % 8 < 3) {
-        newMap[x][y] = color(random(245, 255), random(220, 240), random(0, 25));
-      }
-      else if (y > 34 && x > 22 && x < 28) {
-        newMap[x][y] = color(random(95, 115));
-      }
-      else if (y > 34 && x > 21 && x < 29) {
-        newMap[x][y] = color(random(35, 50));
+function keyPressed() {
+  // Collects all Plants
+  if (keyCode === 32) {
+    for (let x = 0; x < GRID_SIZE; x++) {
+      for (let y = 0; y < 34; y++) {
+        if(maps.data[x][y][1] === 6) {
+          maps.data[x][y] = 0;
+          maps.lobbyMap[x][y] = color(random(220, 230));
+          money += 250;
+        }
       }
     }
   }
-  return newMap;
 }
 
-function genData(rows, cols) {
-  let newMap = [];
-  for (let x = 0; x < rows; x++) {
-    newMap.push([]);
-    for (let y = 0; y < cols; y++) {
-      newMap[x].push([]);
-      if (y < 34 && y > 0) {
-        newMap[x][y] = 1;
-      }
-      else {
-        newMap[x][y] = 0;
-      }
-    }
+function gateCheck() {
+  if(y < 33 && x > 22 && x < 28) {
+    state.identity = "normal";
   }
-
-  return newMap;
+  if(y > 33 && x > 22 && x < 28) {
+    state.identity = "truck";
+  } 
 }
 
-function drawMap(rows, cols) {
-  for (let x = 0; x < rows; x++) {
-    for (let y = 0; y < cols; y++) {
+function draw() {
+  noStroke();
+  drawMap(GRID_SIZE, GRID_SIZE);
+  drawPlayer();
+  fill(0);
+  rect(0, 0, 400, 100);
+  drawBorder();
+  drawMoney();
+  drawPlants();
+}
+
+function drawMap() {
+  for (let x = 0; x < GRID_SIZE; x++) {
+    for (let y = 0; y < GRID_SIZE; y++) {
       fill(maps.lobbyMap[x][y]);
       rect(x * cellSize + xOffset, y * cellSize + yOffset, cellSize, cellSize);
     }
@@ -163,39 +152,23 @@ function drawPlayer() {
   }
 }
 
-function gateCheck() {
-  if(y < 33 && x > 22 && x < 28) {
-    state.identity = "normal";
-  }
-  if(y > 33 && x > 22 && x < 28) {
-    state.identity = "truck";
-  } 
+function drawBorder() {
+  strokeWeight(8);
+  stroke(234, 173, 11);
+  fill(255, 255, 167);
+  rect(4, 4, 29 * money.toString().length + 15, cellSize / 2 + 36);
 }
 
 function drawMoney() {
+  noStroke();
   fill(234, 173, 11);
-  text(money, 2 * cellSize, 2 * cellSize);
+  text(money, 12, 2 * cellSize + 4);
 }
 
-function mousePressed() {
-  let px = floor((mouseX - xOffset) / cellSize);
-  let py = floor((mouseY - yOffset) / cellSize);
-  if(maps.data[px][py][1] === 6) {
-    maps.data[px][py] = 0;
-    maps.lobbyMap[px][py] = color(random(220, 230));
-    money += 250;
-  }
-  else if (!(maps.data[px][py][0] === 2) && py > 0 && py < 34 && money >= 100){
-    maps.data[px][py] = [2, 0];
-    maps.lobbyMap[px][py] = color(plantGrowthStates[0]);
-    money-= 100;
-  }
-}
-
-function stimulatePlants() {
+function drawPlants() {
   for (let x = 0; x < GRID_SIZE; x++) {
     for (let y = 0; y < GRID_SIZE; y++) {
-      if(maps.data[x][y][0] === 2) {
+      if (maps.data[x][y][0] === 2 && random() < 0.05) {
         if (maps.data[x][y][1] < 6) {
           maps.data[x][y][1] += 1;
         }
@@ -203,4 +176,60 @@ function stimulatePlants() {
       }
     }
   }
+}
+
+function genMap() {
+  let newMap = [];
+
+  // lobbyMap Map
+  for (let x = 0; x < GRID_SIZE; x++) {
+    newMap.push([]);
+    for (let y = 0; y < GRID_SIZE; y++) {
+      newMap[x].push([]);
+
+      // Dirt
+      newMap[x][y] = color(random(110, 130), random(40, 50), random(25, 40));
+
+      // lobbyMap
+      if (y < 34 && y > 0) {
+        newMap[x][y] = color(random(220, 230));
+      }
+      else if (y < 35) {
+        newMap[x][y] = color(0);
+      }
+      if (y < 36 && y > 33 && x > 22 && x < 28) {
+        newMap[x][y] = color(50, 84, 48, 50);
+      }
+      
+      // Road
+      if (x === 25 && y > 34 && y % 8 < 3) {
+        newMap[x][y] = color(random(245, 255), random(220, 240), random(0, 25));
+      }
+      else if (y > 34 && x > 22 && x < 28) {
+        newMap[x][y] = color(random(95, 115));
+      }
+      else if (y > 34 && x > 21 && x < 29) {
+        newMap[x][y] = color(random(35, 50));
+      }
+    }
+  }
+  return newMap;
+}
+
+function genData() {
+  let newMap = [];
+  for (let x = 0; x < GRID_SIZE; x++) {
+    newMap.push([]);
+    for (let y = 0; y < GRID_SIZE; y++) {
+      newMap[x].push([]);
+      if (y < 34 && y > 0) {
+        newMap[x][y] = 1;
+      }
+      else {
+        newMap[x][y] = 0;
+      }
+    }
+  }
+
+  return newMap;
 }
